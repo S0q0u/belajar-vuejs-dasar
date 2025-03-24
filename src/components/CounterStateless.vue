e13220893d9ec9d390cd124e27098c88ec33166c
node -v <harlinajengukirasih@gmail.com>
2025-03-24 12:56:21 +0800

    Component Event

--- src/components/CounterStateless.vue
+++ src/components/CounterStateless.vue
@@ -1,8 +1,19 @@
 <script setup>
 // destructuring props, supaya ga perlu tulis props.name, props.counter
-const { name, counter } = defineProps(['name', 'counter']);
+const { name, counter, increment } = defineProps({
+  name: String,
+  counter: Number,
+  increment: Number,
+});
+
+// define emits untuk mengirim event
+const emits = defineEmits(['click']);
 </script>
 
 <template>
-  <h1>counterer {{ name }}: {{ counter }}</h1>
+  <div>
+    <h1>Counter {{ name }}: {{ counter }}</h1>
+    <!-- increment itu event parameter yang dikirim -->
+    <button @click="emits('click', increment)">Increment {{ name }}</button>
+  </div>
 </template>
--- src/components/MultipleCounter.vue
+++ src/components/MultipleCounter.vue
@@ -5,15 +5,40 @@ import { ref } from 'vue';
 
 const counter1 = 'Lin';
 const counter = ref(0);
+
+function increment(value) {
+  counter.value += value;
+}
 </script>
 
 <template>
   <Counter :name="counter1" initial-count="10" />
   <Counter name="Na" initial-count="100" />
 
-  <CounterStateless name="Wawan" :counter="counter" />
-  <CounterStateless name="Xiao" :counter="counter" />
-  <button @click="counter++">Increment Counter</button>
+  <!-- @click nama event yang di emit -->
+  <!-- <CounterStateless
+    name="Wawan"
+    :counter="counter"
+    v-on:click="() => (counter += 1)"
+  />
+  <CounterStateless
+    name="Xiao"
+    :counter="counter"
+    @click="() => (counter += 2)"
+  /> -->
+
+  <CounterStateless
+    name="Wawan"
+    :counter="counter"
+    :increment="1"
+    v-on:click="increment"
+  />
+  <CounterStateless
+    name="Xiao"
+    :counter="counter"
+    :increment="2"
+    @click="increment"
+  />
 </template>
 
 <style scoped></style>
